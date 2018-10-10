d18848fff1040d3c2a7f32ff4d83b0f492904f1d
ManuelR76 <romeromanuel76@gmail.com>
2018-10-10 10:03:37 +0200

    dernière version

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,20 +1,14 @@
-var submit = document.getElementById('submit');//recupère l'id de submit dans le formulaire//
-var firstNumber = document.getElementById('firstNumber');//recupère l'id du premier nombre dans le formulaire//
-var secondNumber = document.getElementById('secondNumber');//recupère l'id du deuxième nombre dans le formulaire//
-var regexDecimal = /^[\d][.,][\d]$/;//défini le regex//
+document.getElementById("submit").addEventListener("click", function () { //création de la fonction après le click sur Valider
 
-submit.addEventListener("click", function () { //création de la fonction après le click sur Valider//
-
-if (regexDecimal.test(firstNumber.value)){ //création des conditions par rapport au regex//
-  if (regexDecimal.test(secondNumber.value)){ //création des conditions par rapport au regex//
-
-      alert(result = Math.trunc(firstNumber.value) * secondNumber.value); //affichage du résultat//
+var firstNumber = document.getElementById("firstNumber").value;//recupère l'id du premier nombre dans le formulaire
+var secondNumber = document.getElementById("secondNumber").value;//recupère l'id du deuxième nombre dans le formulaire
+var regexDecimal = /^[\d]+[.,][\d]+$/;//défini le regex//
+if (regexDecimal.test(firstNumber) && regexDecimal.test(secondNumber)){ //création des conditions par rapport au regex
+  firstNumber = firstNumber.replace(",",".");
+  secondNumber = secondNumber.replace(",",".");
+      alert(result = Math.trunc(firstNumber) * secondNumber); //affichage du résultat
     }
   else {
-    alert("choisissez un nombre décimal pour deuxième nombre");
+    alert("Saisie incorrecte. Veuillez saisir un chiffre positif à virgule");
   }
-}
-else {
-  alert("choisissez un nombre décimal pour premier nombre");
-}
 })
